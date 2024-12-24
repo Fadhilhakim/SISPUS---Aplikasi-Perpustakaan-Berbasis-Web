2e87a6db501c2d468e4f70381dd129bfdc0eefab
Fadhilhakim <fhakim331@gmail.com>
2024-12-24 14:10:21 +0800

    First Deploy

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'DB_HOST' => getenv('MYSQLHOST') ?: 'mysql.railway.internal', // Ganti dengan hostname Railway yang benar
+    'DB_HOST' => getenv('RAILWAY_PRIVATE_DOMAIN') ?: 'mysql.railway.internal',
     'DB_NAME' => getenv('MYSQLDATABASE') ?: 'db_perpustakaan',
     'DB_USER' => getenv('MYSQLUSER') ?: 'root',
     'DB_PASS' => getenv('MYSQLPASSWORD') ?: 'OnexSTQKDlZQeALJQzTYWPCuelMjHhGN',
